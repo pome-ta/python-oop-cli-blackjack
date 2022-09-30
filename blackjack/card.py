66c179b1b8655eba2b50aadd3ee7fc042f0c5c7b
pome-ta <pometarou93@gmail.com>
2022-10-01 00:28:58 +0900

    card

---
+++ blackjack/card.py
@@ -0,0 +1,32 @@
+class Card:
+    __CARD_SCORE = {
+        'A': 1,
+        '2': 2,
+        '3': 3,
+        '4': 4,
+        '5': 5,
+        '6': 6,
+        '7': 7,
+        '8': 8,
+        '9': 9,
+        '10': 10,
+        'J': 10,
+        'Q': 10,
+        'K': 10,
+    }
+
+    __suits = ['スペード', 'ハート', 'ダイヤ', 'クラブ', ]
+
+    def create_new_deck(self) -> list:
+        deck = []
+        for suit in self.__suits:
+            for score, num in self.__CARD_SCORE.items():
+                deck.append({'suit': suit, 'num': num, 'score': score})
+        return deck
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+
+    card = Card()
+    pprint(card.create_new_deck())
